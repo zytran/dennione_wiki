f11715ffe29460e351ac72383ca9195d3be71038
Zy Tran <zrt5062@psu.edu>
2025-12-19 15:00:51 -0500

    persistent storage added

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 //currently uses http://localhost:3000/ for testing
 
 require('dotenv').config();
-
+const path = require("path");
 const express = require('express');
 
 const app = express();
 const port = process.env.port;
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.json());
 
-app.get('/',(req,res)=> {res.render('test_article',{title: "test page", message: "test page works"})})
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname,"views"));
+
+app.get("/", (req, res) => {
+    console.log("route accepted");
+    res.render("test_article", {
+        title: "test page",
+        slug: "test-page",
+        message: "<p>test page works</p>"
+    });
+});
 
 const articleRoutes = require("./routes/wikis");
-app.use(articleRoutes);
+app.use("/", articleRoutes);
 
 app.listen(port, () => {console.log(`server running on port ${port}`)});
--- routes/wikis.js
+++ routes/wikis.js
@@ -1,37 +1,79 @@
-const temp = {};
 const express = require('express');
 const router = express.Router();
+
+const db = require("../db/db");
 
 const marked = require('marked');
 const sanitize = require('sanitize-html');
 
 //view route
-router.get('/wiki/:slug',(req,res)=>{
+router.get('/:slug', async (req,res)=>{
     const slug = req.params.slug; 
-    const markdown = temp[slug] || 'page does not exist';
-    
-    const content = sanitize(marked.parse(markdown));
+    const result = await db.query(
+        "select * from pages where slug = $1",
+        [slug]
+    );
 
-    res.render('test_article',{title:slug.replace(/-/g,' '),message: content});
+    if (result.rows.length===0){
+        return res.render("test_article",{
+            title: slug.replace(/-/g,' '), 
+            message: "<p>page does not exits yet</p>",
+            slug
+        });
+    }
+
+    const page = result.rows[0];
+    
+    
+    const content = sanitize(marked.parse(page.content));
+    const title = sanitize(marked.parse(page.title))
+    res.render("test_article",{
+        title: title,
+        message: content,
+        slug
+    });
+    
 });
 
 //edit route 
-router.get('/wiki/:slug/edit',(req,res)=> {
+router.get('/:slug/edit', async (req,res)=> {
     const slug = req.params.slug;
 
+    const result = await db.query(
+        "select * from pages where slug = $1",
+        [slug]
+    )
+
+    const page = result.rows[0];
+
     res.render('edit',{
-        title: slug.replace(/-/g,' '),
+        title: page?.title || slug.replace(/-/g,' '),
         slug,
-        content: temp[slug] || ''
+        content: page?.content || ''
     });
 
 });
-router.post('/wiki/:slug/edit',(req,res)=> {
-    const slug = req.params.slug;
-    temp[slug] = req.body.content;
 
-    res.redirect(`/wiki/${slug}`);
-})
+router.post('/:slug/edit', async (req,res)=> {
+    const slug = req.params.slug;
+    const title = slug.replace(/-/g," ");
+    const content = req.body.content;
+
+    await db.query(
+        `
+        insert into pages (slug, title, content)
+        values ($1, $2, $3)
+        on conflict (slug)
+        do update set
+            title = excluded.title,
+            content = excluded.content,
+            updated_at = now()
+        `,
+        [slug,title,content]
+    );
+
+    res.redirect(`/${slug}`);
+});
 
 
 module.exports = router;
