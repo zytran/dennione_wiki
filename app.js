8f783947873ec4e5b5da8ad7f600606286b52009
sprYoshingo <zrt5062@psu.edu>
2025-12-21 23:08:50 -0500

    updated package.json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const express = require('express');
 
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
